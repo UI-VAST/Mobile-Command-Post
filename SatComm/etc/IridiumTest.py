03b7af1fbf4475b0be3685c4f41b73dc5609b4a6
gmngmstr <shaunmartin1998@yahoo.com>
2022-02-21 13:16:17 -0800

    Test file added extra output prints

--- SatComm/etc/IridiumTest.py
+++ SatComm/etc/IridiumTest.py
@@ -6,15 +6,16 @@ baud = 19200
 ir = serial.Serial(port, baudrate=baud, timeout=5)
 ir.reset_input_buffer()
 ir.flush()
-
-inp = input()
+print("Starting")
+inp = input("Command: ")
 while inp != 'exit':
     ir.write(bytes(inp, "utf-8"))
     while 1:
         if ir.in_waiting:
+            print("Checking...")
             while ir.in_waiting:
                 temp = ir.readline()
                 print(temp)
                 print(temp.decode())
             break
-    inp = input()
+    inp = input("Command: ")
