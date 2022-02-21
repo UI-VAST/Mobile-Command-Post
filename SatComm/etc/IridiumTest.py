b2ca5dda70a256c6f39d4fc685000f814729220f
gmngmstr <shaunmartin1998@yahoo.com>
2022-02-21 14:06:55 -0800

    Test file New method

--- SatComm/etc/IridiumTest.py
+++ SatComm/etc/IridiumTest.py
@@ -1,23 +1,31 @@
 import serial
 import time
+import board
+from adafruit_rockblock import RockBlock
 
-port = "/dev/serial0"
-baud = 19200
+uart = serial.Serial("/dev/serial0", 19200)
 
-ir = serial.Serial(port, baudrate=baud, timeout=5)
-ir.reset_input_buffer()
-ir.flush()
-print("Starting")
-inp = input("Command: ")
-while inp != 'exit':
-    ir.write(bytes(inp + "\r\n", "utf-8"))
-    while 1:
-        time.sleep(1)
-        if ir.in_waiting:
-            print("Checking...")
-            while ir.in_waiting:
-                temp = ir.readline()
-                print(temp)
-                print(temp.decode())
-            break
-    inp = input("Command: ")
+rb = RockBlock(uart)
+
+print(rb.model)
+
+# port = "/dev/serial0"
+# baud = 19200
+#
+# ir = serial.Serial(port, baudrate=baud, timeout=5)
+# ir.reset_input_buffer()
+# ir.flush()
+# print("Starting")
+# inp = input("Command: ")
+# while inp != 'exit':
+#     ir.write(bytes(inp + "\r", "utf-8"))
+#     while 1:
+#         time.sleep(1)
+#         if ir.in_waiting:
+#             print("Checking...")
+#             while ir.in_waiting:
+#                 temp = ir.readline()
+#                 print(temp)
+#                 print(temp.decode())
+#             break
+#     inp = input("Command: ")
